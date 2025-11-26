52c034603b24432fde86cf185b76f872b7ece394
Amir <152339872+Lapis92@users.noreply.github.com>
2025-11-25 18:18:05 -0800

    Create main.py
    Skeleton for main.py, relies on the creation of sender.py and receiver.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from sender.py import sendMsg
+from recevier.py import viewRecieved
+
+def signInPrompt(primaryUser, otherUser):
+  print("Signed in as " + primaryUser)
+  print("S) Send a message")
+  print("R) Decrypt most recently received message")
+  userAction = input()
+
+  if userAction == 'S':
+    sendMsg(primaryUser, otherUser)
+  elif userAction == 'R':
+    decryptReceived(primaryUser) # this program needs to check if there is a message to read in the first place
+  
+def main:
+  Alice = User()
+  Bob = User()
+  fp = open("Transmitted_Data.txt")
+  fp.write(Alice.getPublicKey())
+  fp.write(Bob.getPublicKey())
+
+  print("Select user")
+  print("A) Alice")
+  print("B) Bob")
+  userSelection = input()
+
+  if userSelection == 'A':
+    signInPrompt(Alice, Bob)
+  elif userSelection == 'B':
+    signInPrompt(Bob, Alice)
+
+    
+  

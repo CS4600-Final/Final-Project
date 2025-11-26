b8f71350b6795e71d5ed43f58b9d786dc31f0fe1
Amir <152339872+Lapis92@users.noreply.github.com>
2025-11-25 17:29:50 -0800

    Create User.py
    Basic skeleton for User class, which will store information and methods for cryptography

---
+++ User.py
@@ -0,0 +1,15 @@
+class User:
+  publicKey = ''
+  privateKey = ''
+
+  def encryptMessage(plaintext):
+    return ciphertext
+
+  def decryptMessage(ciphertext):
+    return plaintext
+
+  def signMessage(plaintext):
+    return signature
+
+  def validateMessage(signature, pubKey):
+    return isValid

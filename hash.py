675a38a06b061cf88ccdc635b2a93dc64cf8de29
Nathanael <nathanaelg2099@gmail.com>
2025-12-02 17:36:49 -0800

    created getHash function to return a hash

--- hash.py
+++ hash.py
@@ -1,4 +1,24 @@
-def genHash():
+from Crypto.Hash import SHA3_256
 
-    return
+def genHash(msg):
+    hashObj = SHA3_256.new()
+
+    for data in msg:
+        hashObj.update(msg)
+
+    return hashObj.hexdigest()
+
+
+
+print("Testing Hash Output")
+msg1 = b"very long string"
+msg2 = b"very long stringg"
+
+hashedMsg1 = genHash(msg1)
+hashedMsg2 = genHash(msg2)
+
+print("Hash of previous message1: " + hashedMsg1)
+print("Hash of previous message2: " + hashedMsg2)
+
+print("Hashes equal? " + (str)(hashedMsg1==hashedMsg2))
 
